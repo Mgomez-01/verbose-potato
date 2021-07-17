891579dbe99fe225b311a78d260148f2efac3da9
Miguel <u1318856@eng.utah.edu>
2021-07-17 16:34:59 +0000

    added more files for the test and the example a.out program

---
+++ example.h
@@ -0,0 +1,15 @@
+// This is a sample program in cpp 
+#include <string>
+
+class test {
+
+    public:
+
+    test();
+    //constructor
+    
+    void doThing(std::string s);
+    private: 
+
+    
+};
