d850304f87b4c720806ee973fea30bece644ca9d
Miguel <u1318856@eng.utah.edu>
2021-07-18 02:44:46 +0000

    Created a place for example and made some updates to the new project script

--- newproj.sh
+++ newproj.sh
@@ -1,8 +1,9 @@
 echo "Setting up folder for project $1"
-
 mkdir $1 
-
 cd $1 
-touch "$1_first.txt" 
+echo "Creating a set of starting files for the project."
+touch "$1_first.txt"
+touch "README.md"
+ 
 
 
